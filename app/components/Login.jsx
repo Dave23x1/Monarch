14ff9ec383e52658d06ae2a11c777032f30261b4
Dave23x1 <129930332+Dave23x1@users.noreply.github.com>
2025-02-27 23:12:49 +0800

    Latest Update

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -2,15 +2,24 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <section className="bg-[#2F2F2F] w-full h-screen">
-      <div className="flex">
-        <div>
-          <div>
+    <section className="bg-[#2F2F2F] w-full h-screen ">
+      <div className="flex ">
+        <div className="">
+          <div className="absolute">
             <Image
               src="/images/BannerCover.jpeg"
               width={450}
               height={340}
               alt="BanncerCover"
+            />
+          </div>
+          <div className="relative">
+            <Image
+              src="/images/Banner.jpeg"
+              width={350}
+              height={250}
+              className="animate-flag"
+              alt="Banner"
             />
           </div>
         </div>
